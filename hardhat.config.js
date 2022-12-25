3d68bf1a1ede70887c295002455817f7debc770d
torback <tlorback@gmail.com>
2022-12-24 23:00:31 -0700

    Update hardhat config, delete starter files, misc small changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,29 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {},
+    // goerli: {
+    //   url: "https://eth-goerli.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
+    //   accounts: [privateKey1, privateKey2, ...]
+    // }
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+}
